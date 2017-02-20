2f3f4d2860ab32989d92e07d9a00db3a4aabf71a
ranjanprj <ranjanprj@users.noreply.github.com>
2017-02-20 15:25:46 +0530

    Euler 1 Rust Solution

---
+++ Euler1_Rust.rs
@@ -0,0 +1,19 @@
+
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+fn euler1_sum() -> i32 {
+    let mut s = 0;
+    for i in 0i32..1000 {
+        if i % 3 == 0 || i % 5 == 0{
+            s += i;
+        }
+    }
+    return s;
+}
+
+fn main() {
+    println!("{}", euler1_sum());
+}
